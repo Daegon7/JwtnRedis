1c75b5aa73a0dc849b329342e766e652a6d9492c
Daegon7 <kdg8302@gmail.com>
2025-11-10 13:22:43 +0900

    Initial commit

--- src/main/java/com/example/auth/controller/UserController.java
+++ src/main/java/com/example/auth/controller/UserController.java
@@ -23,13 +23,11 @@ import java.util.Map;
 
 import org.springframework.http.*;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-
 @Slf4j
 @Tag(name = "User", description = "사용자 관련 API")
 @RestController
 @RequestMapping("/user")
-public class UserController implements GraphQLQueryResolver{
+public class UserController{
 
     private final RestTemplate restTemplate = new RestTemplate();
 
